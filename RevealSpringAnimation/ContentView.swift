47bfbedf7f8035199682e4c36117914fe133434b
CosynPa <v_ds_dt@163.com>
2020-12-20 12:39:37 +0800

    Init

---
+++ RevealSpringAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RevealSpringAnimation
+//
+//  Created by Pan Yusheng on 2020/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RevealSpringAnimation/RevealSpringAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevealSpringAnimationApp.swift
+//  RevealSpringAnimation
+//
+//  Created by Pan Yusheng on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct RevealSpringAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
