c8d626af85cb646608e0d9239aa6b10fa033caaf
CosynPa <v_ds_dt@163.com>
2021-01-17 23:08:53 +0800

    criticalDampingSolve

--- RevealSpringAnimation/Math/SettlingDuratinonSolver.swift
+++ RevealSpringAnimation/Math/SettlingDuratinonSolver.swift
@@ -7,25 +7,58 @@
 
 import Foundation
 
-// Find the largest solution x that |f(x)| = alpha where f is the position from the equilibrium position.
+// Find the largest solution x that |x(x)| = alpha where x is the position from the equilibrium position.
 struct SettlingDurationSolver {
-    private static func criticalDampingSolve(curve: SpringCurve, alpha: Double = 1e-3, epsilon: Double = 1e-8) -> Double {
+    static func criticalDampingSolve(curve: SpringCurve, alpha: Double = 1e-3, epsilon: Double = 1e-8) -> Double {
         assert(curve.dampingRatio == 1.0)
+        assert(alpha < 1)
 
-        let c2 = curve.initialVelocity - curve.omega
+        let v0 = curve.initialVelocity
+        let omega = curve.omega
+
+        let c2 = v0 - omega
 
         if abs(c2) < epsilon {
             // The equation becomes x(t) = -exp(-omega * t)
             return -log(alpha) / curve.omega
         } else {
-            // TODO
-            return 0
+            // x''(t2) = 0, the inflection point
+            let t2 = (2 * c2 + omega) / omega / c2
+
+            if abs(curve.curveFunc(t2) - 1) > alpha {
+                // The sign of x''(t) doesn't change when t > t2, can use Newton's method
+
+                let f: (Double) -> Double
+                if c2 < 0 {
+                    f = { t in curve.curveFunc(t) - 1 + alpha }
+                } else {
+                    f = { t in curve.curveFunc(t) - 1 - alpha }
+                }
+
+                return NewtonSolver.solve(f: f, df: curve.derivativeCurveFunc, x0: t2)
+            } else {
+                // x'(t1) = 0, the turning point
+                let t1 = t2 - 1 / omega
+
+                let f: (Double) -> Double = { t in abs(curve.curveFunc(t) - 1) - alpha }
+
+                if abs(curve.curveFunc(t1) - 1) > alpha {
+                    // |x(t1)| > alpha, |x(t2)| <= alpha, has solution between t1 and t2
+                    // Since t1 is the turning point, the solution is unique
+                    return BinarySearchSolver.solve(f: f, x1: t1, x2: t2)
+                } else {
+                    // |x(0)| = 1 > alpha, |x(t1)| <= alpha, has solution between 0 and t1
+                    // Since t1 is the turning point, the solution is unique
+                    assert(c2 > 0)
+                    return BinarySearchSolver.solve(f: f, x1: 0, x2: t1)
+                }
+            }
         }
     }
 
-    static func settlingDuration(curve: SpringCurve) -> Double {
+    static func settlingDuration(curve: SpringCurve, alpha: Double = 1e-3) -> Double {
         if curve.dampingRatio == 1.0 {
-            return Self.criticalDampingSolve(curve: curve)
+            return Self.criticalDampingSolve(curve: curve, alpha: alpha)
         } else {
             // TODO
             return 0
--- RevealSpringAnimationTests/SettlingDurationSolverTests.swift
+++ RevealSpringAnimationTests/SettlingDurationSolverTests.swift
@@ -10,10 +10,75 @@ import XCTest
 
 class SettlingDurationSolverTests: XCTestCase {
     func testCriticalDamping() throws {
-        let curve = SpringCurve(response: 2 * Double.pi / 2, dampingRatio: 1, initialVelocity: 2)
+        let parameters: [(omega: Double, dampingRatio: Double, v0: Double)] = [
+            (2, 1, 2), // c2 = 0
+            (2, 1, -1), // c2 < 0
+            (2, 1, 4), // c2 > 0
+        ]
 
-        let t = SettlingDurationSolver.settlingDuration(curve: curve)
+        for p in parameters {
+            let curve = SpringCurve(response: 2 * Double.pi / p.omega, dampingRatio: p.dampingRatio, initialVelocity: p.v0)
 
-        XCTAssertEqual(abs(curve.curveFunc(t) - 1), 1e-3, accuracy: 1e-8)
+            let alpha = 1e-3
+            let t = SettlingDurationSolver.criticalDampingSolve(curve: curve, alpha: alpha)
+            testSolution(t: t, curve: curve, alpha: alpha)
+
+            let alpha2 = 0.8
+            let t2 = SettlingDurationSolver.criticalDampingSolve(curve: curve, alpha: alpha2)
+            testSolution(t: t2, curve: curve, alpha: alpha2)
+        }
+    }
+
+    func testCriticalDampingSmallInflectionPoint() throws {
+        let omega = 2.0
+        let curve = SpringCurve(response: 2 * Double.pi / omega, dampingRatio: 1, initialVelocity: 4)
+
+        // The turning point
+        let t1 = 1.0
+        assert(abs(curve.derivativeCurveFunc(t1)) < 1e-6)
+        assert(abs(curve.curveFunc(t1) - 1.1353) < 1e-4)
+
+        // The inflection point
+        let t2 = 1.5
+        assert(abs(curve.curveFunc(t2) - 1.0996) < 1e-4)
+
+        // When the value at t2 is less than alpha
+        let alpha1 = 0.12
+        let t_alpha1 = SettlingDurationSolver.criticalDampingSolve(curve: curve, alpha: alpha1)
+        testSolution(t: t_alpha1, curve: curve, alpha: alpha1)
+
+        // When the value at t1 is less than alpha
+        let alpha2 = 0.15
+        let t_alpha2 = SettlingDurationSolver.criticalDampingSolve(curve: curve, alpha: alpha2)
+        testSolution(t: t_alpha2, curve: curve, alpha: alpha2)
+    }
+
+    func testALot() throws {
+        let omegas = [1.0, 10.0]
+        let dampingRatios = [1.0]
+        let v0s = stride(from: -100.0, through: 100.0, by: 10.0)
+        let alphas = stride(from: 0.001, to: 0.9, by: 0.02)
+
+        for omega in omegas {
+            for d in dampingRatios {
+                for v0 in v0s {
+                    for alpha in alphas {
+                        let curve = SpringCurve(response: 2 * Double.pi / omega, dampingRatio: d, initialVelocity: v0)
+                        let t = SettlingDurationSolver.settlingDuration(curve: curve, alpha: alpha)
+                        testSolution(t: t, curve: curve, alpha: alpha)
+                    }
+                }
+            }
+        }
+
+    }
+
+    func testSolution(t: Double, curve: SpringCurve, alpha: Double) {
+        XCTAssertEqual(abs(curve.curveFunc(t) - 1), alpha, accuracy: 1e-6)
+
+        let step = 0.01 * curve.response
+        for t2 in stride(from: t + step, to: t + 50 * curve.response, by: step) {
+            XCTAssertLessThan(abs(curve.curveFunc(t2) - 1), alpha)
+        }
     }
 }
